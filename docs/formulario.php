86830cf446be7fbb6fe35445045a6bba45f097aa
GabrielLeite27 <75124217+GabrielLeite27@users.noreply.github.com>
2024-09-16 17:46:48 +0000

    certo

--- docs/formulario.php
+++ docs/formulario.php
@@ -19,7 +19,7 @@
         $result = mysqli_query($conexao, "INSERT INTO usuarios(nome,senha,email,telefone,sexo,data_nasc,cidade,estado,endereco) 
         VALUES ('$nome','$senha','$email','$telefone','$sexo','$data_nasc','$cidade','$estado','$endereco')");
 
-        header('Location: index.php');
+        header('Location: index.html');
     }
 
 ?>
